fc46515483f5fe77eafa7b1c4fac87adc7aa2a1e
Vladislav Velichkov <70446196+vipythondi@users.noreply.github.com>
2021-05-25 15:23:17 +0300

    Add files via upload

---
+++ calculator-app-main/js/app.js
@@ -0,0 +1,373 @@
+let one = document.querySelector('.one_checked')
+let two = document.querySelector('.two_checked')
+let three = document.querySelector('.three_checked')
+let page = document.querySelector('.page-wrapper')
+
+one.addEventListener('click', () => {
+    let h3 = document.querySelector('.h3')
+    let theme = document.querySelector('.theme')
+    let numbers = document.querySelector('.header__theme__button__switch__numbers')
+    let slider = document.querySelector('.header__theme__button__switch__slider')
+    let point = document.querySelector('.point')
+    let header_board = document.querySelector('.header__result__board')
+    let board_h1 = document.querySelector('.board_h1')
+    let section__board = document.querySelector('.section__board')
+    let del = document.querySelector('.del')
+    let reset = document.querySelector('.reset')
+    let result = document.querySelector('.result')
+    let result_span = document.querySelector('.result_span')
+    result.style.background = "#D13F30"
+    result.style.boxShadow = "0px 5px #8F2316"
+    result_span.style.color = "white"
+    del.style.background = '#637097'
+    del.style.boxShadow = "0px 5px #414E71"
+    reset.style.background = '#637097'
+    reset.style.boxShadow = "0px 5px  #414E71"
+    section__board.style.background = '#252D44'
+    header_board.style.background = "#181F32"
+    board_h1.style.color = 'white'
+    point.style.background = '#93261A'
+    point.style.marginLeft = "5px"
+    slider.style.background = '#252C46'
+    numbers.style.color = 'white'
+    h3.style.color = 'white';
+    theme.style.color = 'white';
+    page.style.background = '#3B4664'
+
+    // buttons
+    let seven = document.querySelector('.seven')
+    let seven_span = document.querySelector('.seven_span')
+    seven.style.background = "#EAE3DB"
+    seven.style.boxShadow = "0px 5px #A69C92"
+    seven_span.style.color = "#444B5A"
+
+    let eight = document.querySelector('.eight')
+    let eight_span = document.querySelector('.eight_span')
+    eight.style.background = "#EAE3DB"
+    eight.style.boxShadow = "0px 5px #A69C92"
+    eight_span.style.color = "#444B5A"
+
+    let nine = document.querySelector('.nine')
+    let nine_span = document.querySelector('.nine_span')
+    nine.style.background = "#EAE3DB"
+    nine.style.boxShadow = "0px 5px #A69C92"
+    nine_span.style.color = "#444B5A"
+
+    let four = document.querySelector('.four')
+    let four_span = document.querySelector('.four_span')
+    four.style.background = "#EAE3DB"
+    four.style.boxShadow = "0px 5px #A69C92"
+    four_span.style.color = "#444B5A"
+
+    let five = document.querySelector('.five')
+    let five_span = document.querySelector('.five_span')
+    five.style.background = "#EAE3DB"
+    five.style.boxShadow = "0px 5px #A69C92"
+    five_span.style.color = "#444B5A"
+
+    let six = document.querySelector('.six')
+    let six_span = document.querySelector('.six_span')
+    six.style.background = "#EAE3DB"
+    six.style.boxShadow = "0px 5px #A69C92"
+    six_span.style.color = "#444B5A"
+
+    let plus = document.querySelector('.plus')
+    let plus_span = document.querySelector('.plus_span')
+    plus.style.background = "#EAE3DB"
+    plus.style.boxShadow = "0px 5px #A69C92"
+    plus_span.style.color = "#444B5A"
+
+    let one1 = document.querySelector('.one1')
+    let one1_span = document.querySelector('.one1_span')
+    one1.style.background = "#EAE3DB"
+    one1.style.boxShadow = "0px 5px #A69C92"
+    one1_span.style.color = "#444B5A"
+
+    let two2 = document.querySelector('.two2')
+    let two2_span = document.querySelector('.two2_span')
+    two2.style.background = "#EAE3DB"
+    two2.style.boxShadow = "0px 5px #A69C92"
+    two2_span.style.color = "#444B5A"
+
+    let three3 = document.querySelector('.three3')
+    let three3_span = document.querySelector('.three3_span')
+    three3.style.background = "#EAE3DB"
+    three3.style.boxShadow = "0px 5px #A69C92"
+    three3_span.style.color = "#444B5A"
+
+    let minus = document.querySelector('.minus')
+    let minus_span = document.querySelector('.minus_span')
+    minus.style.background = "#EAE3DB"
+    minus.style.boxShadow = "0px 5px #A69C92"
+    minus_span.style.color = "#444B5A"
+
+    let dot = document.querySelector('.dot')
+    let dot_span = document.querySelector('.dot_span')
+    dot.style.background = "#EAE3DB"
+    dot.style.boxShadow = "0px 5px #A69C92"
+    dot_span.style.color = "#444B5A"
+
+    let zero = document.querySelector('.zero')
+    let zero_span = document.querySelector('.zero_span')
+    zero.style.background = "#EAE3DB"
+    zero.style.boxShadow = "0px 5px #A69C92"
+    zero_span.style.color = "#444B5A"
+
+    let dash = document.querySelector('.dash')
+    let dash_span = document.querySelector('.dash_span')
+    dash.style.background = "#EAE3DB"
+    dash.style.boxShadow = "0px 5px #A69C92"
+    dash_span.style.color = "#444B5A"
+
+    let close = document.querySelector('.close')
+    let close_span = document.querySelector('.close_span')
+    close.style.background = "#EAE3DB"
+    close.style.boxShadow = "0px 5px #A69C92"
+    close_span.style.color = "#444B5A"
+})
+
+two.addEventListener('click', () => {
+    let h3 = document.querySelector('.h3')
+    let theme = document.querySelector('.theme')
+    let numbers = document.querySelector('.header__theme__button__switch__numbers')
+    let slider = document.querySelector('.header__theme__button__switch__slider')
+    let point = document.querySelector('.point')
+    let header_board = document.querySelector('.header__result__board')
+    let board_h1 = document.querySelector('.board_h1')
+    let section__board = document.querySelector('.section__board')
+    let del = document.querySelector('.del')
+    let reset = document.querySelector('.reset')
+    let result = document.querySelector('.result')
+    let result_span = document.querySelector('.result_span')
+    result.style.background = "#C85401"
+    result.style.boxShadow = "0px 5px  #873A01"
+    result_span.style.color = "white"
+    del.style.background = '#388187'
+    del.style.boxShadow = "0px 5px #1A5E66"
+    reset.style.background = '#388187'
+    reset.style.boxShadow = "0px 5px #1A5E66"
+    section__board.style.background = '#D3CDCD'
+    header_board.style.background = "#EEEEEE"
+    board_h1.style.color = 'black'
+    point.style.background = '#C75705'
+    point.style.marginLeft = "25px"
+    slider.style.background = 'hsl(0, 5%, 81%)'
+    numbers.style.color = '#52534E'
+    h3.style.color = 'black';
+    theme.style.color = '#52534E';
+    page.style.background = 'hsl(0, 0%, 90%)';
+
+    // buttons
+    let seven = document.querySelector('.seven')
+    let seven_span = document.querySelector('.seven_span')
+    seven.style.background = "#EAE3DB"
+    seven.style.boxShadow = "0px 5px #A69C92"
+    seven_span.style.color = "#444B5A"
+
+    let eight = document.querySelector('.eight')
+    let eight_span = document.querySelector('.eight_span')
+    eight.style.background = "#EAE3DB"
+    eight.style.boxShadow = "0px 5px #A69C92"
+    eight_span.style.color = "#444B5A"
+
+    let nine = document.querySelector('.nine')
+    let nine_span = document.querySelector('.nine_span')
+    nine.style.background = "#EAE3DB"
+    nine.style.boxShadow = "0px 5px #A69C92"
+    nine_span.style.color = "#444B5A"
+
+    let four = document.querySelector('.four')
+    let four_span = document.querySelector('.four_span')
+    four.style.background = "#EAE3DB"
+    four.style.boxShadow = "0px 5px #A69C92"
+    four_span.style.color = "#444B5A"
+
+    let five = document.querySelector('.five')
+    let five_span = document.querySelector('.five_span')
+    five.style.background = "#EAE3DB"
+    five.style.boxShadow = "0px 5px #A69C92"
+    five_span.style.color = "#444B5A"
+
+    let six = document.querySelector('.six')
+    let six_span = document.querySelector('.six_span')
+    six.style.background = "#EAE3DB"
+    six.style.boxShadow = "0px 5px #A69C92"
+    six_span.style.color = "#444B5A"
+
+    let plus = document.querySelector('.plus')
+    let plus_span = document.querySelector('.plus_span')
+    plus.style.background = "#EAE3DB"
+    plus.style.boxShadow = "0px 5px #A69C92"
+    plus_span.style.color = "#444B5A"
+
+    let one1 = document.querySelector('.one1')
+    let one1_span = document.querySelector('.one1_span')
+    one1.style.background = "#EAE3DB"
+    one1.style.boxShadow = "0px 5px #A69C92"
+    one1_span.style.color = "#444B5A"
+
+    let two2 = document.querySelector('.two2')
+    let two2_span = document.querySelector('.two2_span')
+    two2.style.background = "#EAE3DB"
+    two2.style.boxShadow = "0px 5px #A69C92"
+    two2_span.style.color = "#444B5A"
+
+    let three3 = document.querySelector('.three3')
+    let three3_span = document.querySelector('.three3_span')
+    three3.style.background = "#EAE3DB"
+    three3.style.boxShadow = "0px 5px #A69C92"
+    three3_span.style.color = "#444B5A"
+
+    let minus = document.querySelector('.minus')
+    let minus_span = document.querySelector('.minus_span')
+    minus.style.background = "#EAE3DB"
+    minus.style.boxShadow = "0px 5px #A69C92"
+    minus_span.style.color = "#444B5A"
+
+    let dot = document.querySelector('.dot')
+    let dot_span = document.querySelector('.dot_span')
+    dot.style.background = "#EAE3DB"
+    dot.style.boxShadow = "0px 5px #A69C92"
+    dot_span.style.color = "#444B5A"
+
+    let zero = document.querySelector('.zero')
+    let zero_span = document.querySelector('.zero_span')
+    zero.style.background = "#EAE3DB"
+    zero.style.boxShadow = "0px 5px #A69C92"
+    zero_span.style.color = "#444B5A"
+
+    let dash = document.querySelector('.dash')
+    let dash_span = document.querySelector('.dash_span')
+    dash.style.background = "#EAE3DB"
+    dash.style.boxShadow = "0px 5px #A69C92"
+    dash_span.style.color = "#444B5A"
+
+    let close = document.querySelector('.close')
+    let close_span = document.querySelector('.close_span')
+    close.style.background = "#EAE3DB"
+    close.style.boxShadow = "0px 5px #A69C92"
+    close_span.style.color = "#444B5A"
+})
+
+three.addEventListener('click', () => {
+    let h3 = document.querySelector('.h3')
+    let theme = document.querySelector('.theme')
+    let numbers = document.querySelector('.header__theme__button__switch__numbers')
+    let slider = document.querySelector('.header__theme__button__switch__slider')
+    let point = document.querySelector('.point')
+    let header_board = document.querySelector('.header__result__board')
+    let board_h1 = document.querySelector('.board_h1')
+    let section__board = document.querySelector('.section__board')
+    let del = document.querySelector('.del')
+    let reset = document.querySelector('.reset')
+    let result = document.querySelector('.result')
+    let result_span = document.querySelector('.result_span')
+    // buttons
+    let seven = document.querySelector('.seven')
+    let seven_span = document.querySelector('.seven_span')
+    seven.style.background = "#331B4D"
+    seven.style.boxShadow = "0px 5px #811F9A"
+    seven_span.style.color = "#FCE74B"
+
+    let eight = document.querySelector('.eight')
+    let eight_span = document.querySelector('.eight_span')
+    eight.style.background = "#331B4D"
+    eight.style.boxShadow = "0px 5px #811F9A"
+    eight_span.style.color = "#FCE74B"
+
+    let nine = document.querySelector('.nine')
+    let nine_span = document.querySelector('.nine_span')
+    nine.style.background = "#331B4D"
+    nine.style.boxShadow = "0px 5px #811F9A"
+    nine_span.style.color = "#FCE74B"
+
+    let four = document.querySelector('.four')
+    let four_span = document.querySelector('.four_span')
+    four.style.background = "#331B4D"
+    four.style.boxShadow = "0px 5px #811F9A"
+    four_span.style.color = "#FCE74B"
+
+    let five = document.querySelector('.five')
+    let five_span = document.querySelector('.five_span')
+    five.style.background = "#331B4D"
+    five.style.boxShadow = "0px 5px #811F9A"
+    five_span.style.color = "#FCE74B"
+
+    let six = document.querySelector('.six')
+    let six_span = document.querySelector('.six_span')
+    six.style.background = "#331B4D"
+    six.style.boxShadow = "0px 5px #811F9A"
+    six_span.style.color = "#FCE74B"
+
+    let plus = document.querySelector('.plus')
+    let plus_span = document.querySelector('.plus_span')
+    plus.style.background = "#331B4D"
+    plus.style.boxShadow = "0px 5px #811F9A"
+    plus_span.style.color = "#FCE74B"
+
+    let one1 = document.querySelector('.one1')
+    let one1_span = document.querySelector('.one1_span')
+    one1.style.background = "#331B4D"
+    one1.style.boxShadow = "0px 5px #811F9A"
+    one1_span.style.color = "#FCE74B"
+
+    let two2 = document.querySelector('.two2')
+    let two2_span = document.querySelector('.two2_span')
+    two2.style.background = "#331B4D"
+    two2.style.boxShadow = "0px 5px #811F9A"
+    two2_span.style.color = "#FCE74B"
+
+    let three3 = document.querySelector('.three3')
+    let three3_span = document.querySelector('.three3_span')
+    three3.style.background = "#331B4D"
+    three3.style.boxShadow = "0px 5px #811F9A"
+    three3_span.style.color = "#FCE74B"
+
+    let minus = document.querySelector('.minus')
+    let minus_span = document.querySelector('.minus_span')
+    minus.style.background = "#331B4D"
+    minus.style.boxShadow = "0px 5px #811F9A"
+    minus_span.style.color = "#FCE74B"
+
+    let dot = document.querySelector('.dot')
+    let dot_span = document.querySelector('.dot_span')
+    dot.style.background = "#331B4D"
+    dot.style.boxShadow = "0px 5px #811F9A"
+    dot_span.style.color = "#FCE74B"
+
+    let zero = document.querySelector('.zero')
+    let zero_span = document.querySelector('.zero_span')
+    zero.style.background = "#331B4D"
+    zero.style.boxShadow = "0px 5px #811F9A"
+    zero_span.style.color = "#FCE74B"
+
+    let dash = document.querySelector('.dash')
+    let dash_span = document.querySelector('.dash_span')
+    dash.style.background = "#331B4D"
+    dash.style.boxShadow = "0px 5px #811F9A"
+    dash_span.style.color = "#FCE74B"
+
+    let close = document.querySelector('.close')
+    let close_span = document.querySelector('.close_span')
+    close.style.background = "#331B4D"
+    close.style.boxShadow = "0px 5px #811F9A"
+    close_span.style.color = "#FCE74B"
+
+    result_span.style.color = 'black'
+    result.style.background = "#00DECF"
+    result.style.boxShadow = "0px 5px #76F3EF"
+    del.style.background = '#56077C'
+    del.style.boxShadow = "0px 5px #B51FE6"
+    reset.style.background = '#56077C'
+    reset.style.boxShadow = "0px 5px #B51FE6"
+    section__board.style.background = '#1E0836'
+    header_board.style.background = "#1E0836"
+    board_h1.style.color = '#FBE353'
+    point.style.background = '#07DBD4'
+    point.style.marginLeft = "50px"
+    slider.style.background = '#1E0836'
+    numbers.style.color = '#FBE353'
+    h3.style.color = '#FBE353';
+    theme.style.color = '#FBE353';
+    page.style.background = 'hsl(268, 75%, 9%)'
+})
